af5f7dafb3e3ef75215c4d3b09473d904b8aa0ec
Dencina1996 <davidencina1996@gmail.com>
2018-09-28 18:52:20 +0200

    Third Commit

--- Calendari.php
+++ Calendari.php
@@ -16,27 +16,35 @@
 		</tr>
 		<?php
 
-			$First_Day_Start = 5;
+			$First_Day_Start = 0;
 			$First_Day = date('d', strtotime('first day of this month'));
 			$Last_Day = date('d', strtotime('last day of this month'));
 			$Today = date('d', strtotime('today'));
-			$Columna = 0;
+			$Columna = 5;
 
 			for ($i = $First_Day; $i <= $Last_Day; $i ++) {
 
-				if ($Columna <=6) {
+				if ($First_Day_Start==5) {
 
-					$Columna++;				
-					echo "<td>$i</td>";
+					if ($Columna <=6) {
 
+						$Columna++;				
+						echo "<td>$i</td>";
+
+					} else {
+
+						echo "<tr>";
+						$Columna = 0;
+
+					}
+					
 				} else {
 
-					echo "<tr>";
-					$Columna = 0;
+					echo "<td></td>";
+					$First_Day_Start++;
 
 				}
-				
-			}
+		}
 			
 		?>
 	</table>
